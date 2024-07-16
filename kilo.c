67bf7e813fe55c89e8c6f8f6790510d82b4d9245
obese-penguin <sketchyakounts@gmail.com>
2024-07-16 13:18:24 +0530

    exit function and clear screen

--- kilo.c
+++ kilo.c
@@ -1,19 +1,40 @@
+/*** includes ***/
+
 #include<stdio.h>
 #include<unistd.h>
 #include<termios.h>
 #include<stdlib.h>
 #include<ctype.h>
+#include<errno.h>
+
+/*** defines ***/
+#define CTRL_KEY(k) ((k) & 0x1f)
+
+
+/*** data ***/
 
 struct termios orig_termios;
 
+/*** terminal ***/
+
+void die(const char* string)
+{
+    perror(string);
+
+    exit(1);
+}
+
 void disableRawMode()
 {
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+    // to restore original terminal attributes on exit
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1) 
+        die("tcsetattr");
 }
 
 void enableRawMode()
 {
-    tcgetattr(STDIN_FILENO, &orig_termios);
+    if (tcgetattr(STDIN_FILENO, &orig_termios) == -1)
+        die("tcgetattr");
     
     atexit(disableRawMode);
 
@@ -31,31 +52,55 @@ void enableRawMode()
     // IEXTEN -- ctrl+v or ctrl+o. not sure about this.
     // OPOST -- disabled translation of '\n' to '\r\n'
 
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) 
+        die("tcsetattr");
 }
+
+char editorReadKey()
+{
+    int nread;
+    char c;
+
+    while((nread = read(STDIN_FILENO, &c, 1)) != 1)
+    {
+        if(nread == -1 && errno != EAGAIN) die("read"); // errno != EAGAIN is for cygwin
+    }
+
+    return c;
+}
+
+/*** output  ***/
+void editorRefreshScreen()
+{
+    write(STDOUT_FILENO, "\x1b[2J", 4); // that is the sequence to clear screen. 
+    write(STDOUT_FILENO, "\x1b[H", 3); // send cursor to top of the screen. 
+}
+
+/*** input  ***/
+
+void editorProcessKeypress()
+{
+    char c = editorReadKey();
+
+    switch (c)
+    {
+        case CTRL_KEY('q'):
+            exit(0);
+            break;
+    }
+}
+
+/*** init ***/
 
 int main()
 {
+    editorRefreshScreen();
+
     enableRawMode();
 
-    char c;
     while(1)
     {
-        char c = '\0';
-        read(STDIN_FILENO, &c, 1);
-
-        if (iscntrl(c))
-        {
-            printf("%d\r\n", c); 
-        }
-
-        else 
-        {
-            printf("%d, ('%c')\r\n", c, c);
-        }
-
-        if(c == 'q')
-            break;
+        editorProcessKeypress();
     }
     return 0;
 }
