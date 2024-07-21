74a3ff8e06f7262dc09ad04a4f34e789cc3b3d65
obese-penguin <sketchyakounts@gmail.com>
2024-07-21 12:38:06 +0530

    added support for home, pgup, pgdwn, del keys

--- kilo.c
+++ kilo.c
@@ -17,7 +17,12 @@ enum editorKey {
     ARROW_UP = 1000,
     ARROW_DOWN,
     ARROW_RIGHT,
-    ARROW_LEFT
+    ARROW_LEFT,
+    PAGE_UP,
+    PAGE_DOWN,
+    HOME_KEY,
+    END_KEY,
+    DEL_KEY
 };
 
 /*** data ***/
@@ -115,16 +120,47 @@ int editorReadKey()
 
         if(seq[0] == '[')
         {
+            if(seq[1] >= '0' && seq[1] <= '9')
+            {
+                if(read(STDIN_FILENO, &seq[2], 1) != 1) return '\x1b';
+                if(seq[2] == '~')
+                {
+                    switch (seq[1])
+                    {
+                        case '1': return HOME_KEY;
+                        case '3': return DEL_KEY;
+                        case '4': return END_KEY;
+                        case '5': return PAGE_UP;
+                        case '6': return PAGE_DOWN;
+                        case '7': return HOME_KEY;
+                        case '8': return END_KEY;
+                    }
+                }
+            }
+
+            else 
+            {
+                switch(seq[1])
+                {
+                    case 'A': return ARROW_UP;
+                    case 'B': return ARROW_DOWN;
+                    case 'C': return ARROW_RIGHT;
+                    case 'D': return ARROW_LEFT;
+                    case 'H': return HOME_KEY;
+                    case 'F': return END_KEY;
+                }
+            }
+        }
+
+        else if(seq[0] == 'O')
+        {
             switch(seq[1])
             {
-                case 'A': return ARROW_UP;
-                case 'B': return ARROW_DOWN;
-                case 'C': return ARROW_RIGHT;
-                case 'D': return ARROW_LEFT;
+                case 'H': return HOME_KEY;
+                case 'F': return END_KEY;
             }
         }
     }
-
     return c;
 }
 
@@ -248,16 +284,20 @@ void editorMoveCursor(int c)
     switch (c)
     {
         case ARROW_UP:
-            E.cy -= 1;
+            if(E.cy != 0)
+                E.cy -= 1;
             break;
         case ARROW_LEFT:
-            E.cx -= 1;
+            if(E.cx != 0)
+                E.cx -= 1;
             break;
         case ARROW_DOWN:
-            E.cy += 1;
+            if(E.cy != E.screenrows-1)
+                E.cy += 1;
             break;
         case ARROW_RIGHT:
-            E.cx += 1;
+            if(E.cx != E.screencols-1)
+                E.cx += 1;
             break;
     }
 }
@@ -273,12 +313,30 @@ void editorProcessKeypress()
             write(STDOUT_FILENO, "\x1b[H", 3); 
             exit(0); // 0 for success; 1 for failure
             break;
+    
+        case HOME_KEY:
+            E.cx = 0;
+            break;
 
+        case END_KEY:
+            E.cx = E.screencols - 1;
+            break;
+
+        case PAGE_UP:
+        case PAGE_DOWN:
+            {
+                int times = E.screenrows;
+                while (times--)
+                {
+                    editorMoveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
+                }
+                break;
+            }
         case ARROW_UP:
         case ARROW_DOWN:
         case ARROW_RIGHT:
         case ARROW_LEFT:
-           editorMoveCursor(c);
+            editorMoveCursor(c);
             break; 
     }
 }
